1aff6c1c7d7363d33d6152a92aa5c78c13481a25
Redouane Maamri <maamriredouane9@gmail.com>
2025-10-16 21:04:23 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Header with image + title on same line */}
+      <View style={styles.header}>
+        <Image
+          source={require('./assets/Emsi.jpeg')}
+          style={styles.image}
+        />
+        <Text style={styles.title}>STUDENT CARD EMSI CENTRE</Text>
+      </View>
+
+      {/* Student info */}
+      <Text style={styles.label}>
+        Nom : <Text style={styles.value}>TENSAOUI</Text>
+      </Text>
+      <Text style={styles.label}>
+        Prénom : <Text style={styles.value}>ALI</Text>
+      </Text>
+      <Text style={styles.label}>
+        Année universitaire : <Text style={styles.value}>2025 / 2026</Text>
+      </Text>
     </View>
   );
 }
@@ -13,8 +29,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  header: {
+    flexDirection: 'row', 
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  image: {
+    width: 130,
+    height: 60,
+    borderRadius: 10,
+    marginRight: 10,
+    borderRadius: 10,
+    marginRight: 15,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#004aad',
+  },
+  label: {
+    fontSize: 16,
+    color: '#333',
+    marginVertical: 4,
+  },
+  value: {
+    color: 'blue',
+    fontWeight: 'bold',
   },
 });
